34a523313728f9b839cf0c2c316ad7d574ce08b1
qetuop <newsgrpacc@yahoo.com>
2017-06-17 16:44:13 -0400

    - adding some backend

--- app/src/main/java/com/qetuop/weighttracker/MainActivity.java
+++ app/src/main/java/com/qetuop/weighttracker/MainActivity.java
@@ -1,13 +1,46 @@
 package com.qetuop.weighttracker;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.text.DateFormat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText weightET;
+    private EditText dateET;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
+        String timeStamp = new SimpleDateFormat("MM/dd/yyyy").format(new Date());
+
+        weightET = (EditText)findViewById(R.id.weightET);
+        dateET = (EditText)findViewById(R.id.dateET);
+
+        dateET.setText(timeStamp);
     }
+
+    public void okClicked(View view){
+        String weight = weightET.getText().toString();
+        System.out.println(weight);
+
+        String date = dateET.getText().toString();
+        System.out.println(date);
+
+        Toast.makeText(this, "Weight=", Toast.LENGTH_SHORT).show();
+
+    }
+
+
+
+
 }
